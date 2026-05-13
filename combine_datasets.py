43d4aa0e507cb37b96d27c25524d53260240c50f
SHIROKAMIQQ <migzguiang@gmail.com>
2026-05-13 23:52:10 +0800

    fix(combine): remove master.csv before reading all csv files

--- combine_datasets.py
+++ combine_datasets.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import glob
+import os
 
 """
 We have two folders of data: chatter_classifier_datasets and duration_prediction_datasets.
 We want to combine the datasets in each folder such that each folder will have a master.csv file.
 This master.csv file will be the one used by the models.
 """
-
+os.remove("chatter_classifier_datasets/master.csv")
 csv_folder = glob.glob("chatter_classifier_datasets/*.csv")
 df_list = [pd.read_csv(file) for file in csv_folder]
 master_df = pd.concat(df_list, ignore_index=True)
 master_df.to_csv("chatter_classifier_datasets/master.csv", index=False)
 print("Combined all .csv files in chatter_classifier_datasets/ into chatter_classifier_datasets/master.csv")
 
-
+os.remove("duration_prediction_datasets/master.csv")
 csv_folder = glob.glob("duration_prediction_datasets/*.csv")
 df_list = [pd.read_csv(file) for file in csv_folder]
 master_df = pd.concat(df_list, ignore_index=True)
